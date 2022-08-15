1a2279badeb29d1efbbcdde2579291cd1b562240
f-starace <64803153+f-starace@users.noreply.github.com>
2022-08-15 23:31:46 +0200

    removed old files. moved example.tsx to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,86 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Example from "./example";
 import "./index.css";
+import { ColumnDef } from "@tanstack/react-table";
+import defaultData, { Person } from "./sampleData";
+import TTTable from "./TTTable";
+
+const Example = () => {
+  const columns = React.useMemo<ColumnDef<Person, any>[]>(
+    () => [
+      {
+        header: "Name",
+        footer: (props) => props.column.id,
+        columns: [
+          {
+            accessorKey: "firstName",
+            cell: (info) => info.getValue(),
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorFn: (row) => row.lastName,
+            id: "lastName",
+            cell: (info) => info.getValue(),
+            header: () => <span>Last Name</span>,
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorFn: (row) => `${row.firstName} ${row.lastName}`,
+            id: "fullName",
+            header: "Full Name",
+            cell: (info) => info.getValue(),
+            footer: (props) => props.column.id,
+          },
+        ],
+      },
+      {
+        header: "Info",
+        footer: (props) => props.column.id,
+        columns: [
+          {
+            accessorKey: "age",
+            header: () => "Age",
+            footer: (props) => props.column.id,
+          },
+          {
+            header: "More Info",
+            columns: [
+              {
+                accessorKey: "visits",
+                header: () => <span>Visits</span>,
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "status",
+                header: "Status",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "Profile Progress",
+                footer: (props) => props.column.id,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    []
+  );
+
+  return (
+    <TTTable
+      condensed
+      striped
+      bordered
+      inputData={defaultData}
+      inputColumns={columns}
+      pagination
+      resizable
+      sticky
+    />
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
